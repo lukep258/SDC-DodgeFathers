0806ae3070447d506fe3d68d9bb93fc3f719d09a
Patino5 <106916823+Patino5@users.noreply.github.com>
2024-01-02 15:06:14 -0600

    Image and text added to component

---
+++ cobra/src/components/Fact4.jsx
@@ -0,0 +1,24 @@
+
+
+const Fact4 = () => {
+
+    return (
+        <div className="card lg:card-side bg-base-100 flex items-center justify-center ">
+            <figure>
+                <img src="http://images.salsify.com/image/upload/s--LE1jbPHA--/ffr9ld5se4hnncjrzrul.png" 
+                alt="Unlock Faster Ball Speed" 
+                />  
+            </figure>
+            <div className="card-body grid__item order--desktop-1 txtcol">
+                <h1 className="card-title text-2xl mb-2">Unlock Faster Ball Speed</h1>
+                <p className="w-3/6 text-base">Forward positioning of the fully suspended PWR-BRIDGE weight allows for maximum energy transfer to unleash faster ball speeds through unrestricted flexibility of the face and sole.</p>
+            </div>
+        </div>
+    )
+} 
+
+export default Fact4
+
+// <div className="sustainability__wrapper page-width color-background-1 gradient scroll-trigger animate--fade-in">
+
+// </div>
