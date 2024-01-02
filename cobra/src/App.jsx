eee634a1ceb2cb9e7c342b28c31b33863b2f5ed7
Michael Mote <michael.mote1993@gmail.com>
2024-01-02 14:21:25 -0600

    Images Added and Functional

--- cobra/src/App.jsx
+++ cobra/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+<<<<<<< HEAD
 import FactTwo from './components/FactTwo'
 import FactThree from './components/FactThree'
 
+=======
+import Carousel from './components/Carousel'
+>>>>>>> 925e1e8 (Images Added and Functional)
 
 import './App.css'
 
@@ -12,8 +16,13 @@ function App() {
   return (
     <>
       <Navbar />
+<<<<<<< HEAD
       <FactTwo/>
       <FactThree/>
+=======
+      {/* Mote's Carousel below, move where necessary */}
+      <Carousel /> 
+>>>>>>> 925e1e8 (Images Added and Functional)
     </>
   )
 }
--- cobra/src/components/Carousel.jsx
+++ cobra/src/components/Carousel.jsx
@@ -2,8 +2,26 @@ import React from "react";
 
 const Carousel = () => {
     return (
-        <>
-        </>
+        <div className="carousel carousel-center">
+            <div className="carousel-item">
+                <img src="https://images.salsify.com/image/upload/s--rzT9cPwv--/d5125613f56e924a328227b56f72bcd9bf9ccdf4.jpg" alt="1stCarouselItem" />
+            </div>
+            <div className="carousel-item">
+                <img src="https://images.salsify.com/image/upload/s--BZhdhI1F--/c3686830477be0b16a2bb142544168ffca185566.jpg" alt="2ndCarouselItem" />
+            </div>
+            <div className="carousel-item">
+                <img src="https://images.salsify.com/image/upload/s--k_fiw7Y1--/9ccb037712039b3cd60e0e7f6f8fd2da0370802c.jpg" alt="3rdCarouselItem" />
+            </div>
+            <div className="carousel-item">
+                <img src="https://images.salsify.com/image/upload/s--k_fiw7Y1--/9ccb037712039b3cd60e0e7f6f8fd2da0370802c.jpg" alt="4thCarouselItem" />
+            </div>
+            <div className="carousel-item">
+                <img src="https://images.salsify.com/image/upload/s--RbmxuiJv--/f3bb4f9f1a52b17e9dc301078593f711ce173a28.jpg" alt="5thCarouselItem" />
+            </div>
+            <div className="carousel-item">
+                <img src="	https://images.salsify.com/image/upload/s--3W2-ylQs--/cda6b63d593e850b5ef8c7922830e82422a40667.jpg" alt="6thCarouselItem" />
+            </div>
+        </div>
     )
 }
 
