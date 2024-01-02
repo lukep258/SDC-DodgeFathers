05541d37afd4301f0a1c9c210532ce006f4050b8
Michael Mote <michael.mote1993@gmail.com>
2024-01-02 14:21:25 -0600

    Images Added and Functional

--- cobra/src/App.jsx
+++ cobra/src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
-<<<<<<< HEAD
 import FactTwo from './components/FactTwo'
 import FactThree from './components/FactThree'
 
-=======
-import Carousel from './components/Carousel'
->>>>>>> 925e1e8 (Images Added and Functional)
 
 import './App.css'
 
@@ -16,13 +12,8 @@ function App() {
   return (
     <>
       <Navbar />
-<<<<<<< HEAD
       <FactTwo/>
       <FactThree/>
-=======
-      {/* Mote's Carousel below, move where necessary */}
-      <Carousel /> 
->>>>>>> 925e1e8 (Images Added and Functional)
     </>
   )
 }
