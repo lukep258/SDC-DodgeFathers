ace8c37a1a287cff5dddca19fcef617c80938b3f
Michael Mote <michael.mote1993@gmail.com>
2024-01-02 17:47:25 -0600

    Add FactOne component and custom-hero class

--- cobra/src/App.jsx
+++ cobra/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Carousel from './components/Carousel'
 
 import './App.css'
+import FactOne from './components/FactOne'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,8 @@ function App() {
       <Navbar />
       {/* Mote's Carousel below, move where necessary */}
       <Carousel /> 
+      {/* Mote's FactOne component below, move where necessary */}
+      <FactOne />
     </>
   )
 }
