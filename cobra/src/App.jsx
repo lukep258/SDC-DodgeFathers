fab4b7b61545350676f51ab1794b95de9243d017
S-Hinkle <shcatchall@protonmail.com>
2024-01-02 17:36:18 -0500

    Initial Header component creation

--- cobra/src/App.jsx
+++ cobra/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Header from './components/Header'
 import Navbar from './components/Navbar'
 
 import './App.css'
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+      <Header />
       <Navbar />
     </>
   )
--- cobra/src/components/Header.jsx
+++ cobra/src/components/Header.jsx
@@ -1,10 +1,22 @@
 
 export default function Header() {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+        <div className="grid grid-cols-3 gap-4 bg-gray-100">
+            <div role="tablist" className="flex content-start tabs ml-10">
+                <a role="tab" className="tab tab-active font-bold bg-base-100" href="https://cobragolf.com/" aria-label="https://cobragolf.com/">
+                    COBRA&nbsp;
+                    <span className="font-normal">
+                        GOLF
+                    </span>
+                </a>
+                <a role="tab" className="tab tab-active font-bold hover:bg-gray-200" href="https://pumagolf.com" aria-label="https://pumagolf.com">
+                    PUMA&nbsp;
+                    <span className="font-normal">
+                        GOLF
+                    </span>
+                </a>
+            </div>
+            <div className="flex justify-center font-light">FREE SHIPPING OVER $50</div>
         </div>
     )
 }
