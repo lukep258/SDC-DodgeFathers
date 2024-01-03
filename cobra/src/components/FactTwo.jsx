e72eb9ba72bed7d1a92491e2e583278764f25cf4
WRhee123 <rhee.william@yahoo.com>
2024-01-03 12:53:34 -0500

    FactTwo done

--- cobra/src/components/FactTwo.jsx
+++ cobra/src/components/FactTwo.jsx
@@ -4,11 +4,11 @@ const FactTwo = () => {
         <>
         <div className="flex flex-wrap justify-center items-center px-10 py-10">
             <div>
-                <img className="mr-40" src="http://images.salsify.com/image/upload/s--_4se3riN--/efyy1hkdejstekvzkh5v.png" loading='lazy' alt='Swing Your Fastest'>
+                <img className="mr-20 ml-44" src="http://images.salsify.com/image/upload/s--_4se3riN--/efyy1hkdejstekvzkh5v.png" loading='lazy' alt='Swing Your Fastest'>
                 </img>
             </div>
-            <div className=" flex flex-col justify-start w-80 grow">
-                <h1 className ="text-2xl font-medium">Swing Your Fastest</h1>
+            <div className=" flex flex-col justify-start w-80 grow mr-20 ml-10">
+                <h1 className ="text-3xl font-medium mb-8">Swing Your Fastest</h1>
                 <div>
                 Validated by the fastest players in the world The AEROJET Drivers features a raised skirt, 
                 symmetrical shaping, and streamlined edges to give you the most speed possible. 
