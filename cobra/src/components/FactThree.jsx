c15790dc814c928044ecaa06ed4f5364e05b8567
WRhee123 <rhee.william@yahoo.com>
2024-01-03 13:01:33 -0500

    FactThree margins fixed

--- cobra/src/components/FactThree.jsx
+++ cobra/src/components/FactThree.jsx
@@ -1,16 +1,16 @@
 const FactThree = () => {
 
 return (<>
-<div className="flex flex-wrap justify-center items-center">
-    <div className="flex flex-col justify-center items-center w-80 grow">
-        <h1 className ="text-2xl font-medium">Higher Launch. Optimized Spin.</h1>
+<div className="flex flex-wrap justify-center items-center px-10 py-10">
+    <div className="flex flex-col justify-start w-80 grow mr-20 ml-10">
+        <h1 className ="text-3xl font-medium mb-6">Higher Launch. Optimized Spin.</h1>
         <div>
         Proven PWRSHELL L-cup technology maximizes launch and distance, 
         while an A.I. designed H.O.T Face delivers more speed and efficient spin across the entire clubface.
         </div>
     </div>
-    <div className='ml-5 grow'>
-        <img src="http://images.salsify.com/image/upload/s--qMpXoeFa--/qddwahsruo43snvdwcnk.png" loading="lazy" alt="Higher Launch. Optimized Spin.">
+    <div className='ml-20 grow'>
+        <img className="ml-10" src="http://images.salsify.com/image/upload/s--qMpXoeFa--/qddwahsruo43snvdwcnk.png" loading="lazy" alt="Higher Launch. Optimized Spin.">
             </img>
     </div>
     
