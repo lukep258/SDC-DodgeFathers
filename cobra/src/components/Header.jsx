f6bda9991d5ee02614b2648dc1f97dd16a0426ec
S-Hinkle <shcatchall@protonmail.com>
2024-01-03 12:33:07 -0500

    Header updated just requires fonts

--- cobra/src/components/Header.jsx
+++ cobra/src/components/Header.jsx
@@ -1,22 +1,16 @@
 
 export default function Header() {
     return (
-        <div className="grid grid-cols-3 gap-4 bg-gray-100">
-            <div role="tablist" className="flex content-start tabs ml-10">
-                <a role="tab" className="tab tab-active font-bold bg-base-100" href="https://cobragolf.com/" aria-label="https://cobragolf.com/">
-                    COBRA&nbsp;
-                    <span className="font-normal">
-                        GOLF
-                    </span>
+        <div className="lg:grid lg:grid-cols-3 flex flex-row justify-between items-center gap-4 bg-gray-100 px-2">
+            <div role="tablist" className="flex flex-row gap-2">
+                <a role="tab" className="tab tab-active font-bold bg-base-100 md:ml-8" href="https://cobragolf.com/" aria-label="https://cobragolf.com/">
+                    COBRA&nbsp;<span className="font-normal md:inline hidden">GOLF</span>
                 </a>
-                <a role="tab" className="tab tab-active font-bold hover:bg-gray-200" href="https://pumagolf.com" aria-label="https://pumagolf.com">
-                    PUMA&nbsp;
-                    <span className="font-normal">
-                        GOLF
-                    </span>
+                <a role="tab" className="tab font-bold hover:bg-gray-200" href="https://pumagolf.com" aria-label="https://pumagolf.com">
+                    PUMA&nbsp;<span className="font-normal md:inline hidden">GOLF</span>
                 </a>
             </div>
-            <div className="flex justify-center font-light">FREE SHIPPING OVER $50</div>
+            <div className="flex lg:justify-center md:justify-center sm:justify-end font-light">FREE SHIPPING OVER $50</div>
         </div>
     )
 }
