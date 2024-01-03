7635decfacb76d6ae834182c21e3e8e3a8c796e0
Michael Mote <michael.mote1993@gmail.com>
2024-01-03 14:04:05 -0600

    Add Carousel component to App.jsx

--- cobra/src/App.jsx
+++ cobra/src/App.jsx
@@ -27,7 +27,8 @@ function App() {
                 <Navbar />
                 <Product />
                 <FactTwo/>
-                <FactThree/>
+                <FactThree />
+                <Carousel />
             </>
     </>
   );
