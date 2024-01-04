234083993f752bac1e505c253b12c8d93c038854
Michael Mote <michael.mote1993@gmail.com>
2024-01-04 11:38:33 -0600

    Refactor hero section styling for FactOne component

--- cobra/src/components/FactOne.jsx
+++ cobra/src/components/FactOne.jsx
@@ -1,13 +1,19 @@
 
 const FactOne = () => {
     return (
-        <div className="hero custom-hero">
-            <div className="hero-content py-5 flex">
-                <div className="leading-5">Faster By Design</div>
-                <p>
-                    Seamlessly aerodynamic, bodly designed. COBRA AEROJET enables
-                    new levels of speed that were not believed to be possible until now.
-                </p>
+        // background image styling done inline for ease of access ( api fetches for image urls )
+        // styling is mixed between inline and index.css for responsiveness
+        // if you want to refactor styling using tailwind, research tailwind's utility classes
+        <div className="hero custom-hero" style={{backgroundImage: 'url(http://images.salsify.com/image/upload/s--35mrsC77--/7b8cc3ac2424f7186070c5e491b2b761bf5eb250.jpg)'}}>
+            <div className="hero-content-wrapper">
+                <div className="hero-content" style={{width: '80%', padding: '0 100px'}}>
+                    <div className="hero-content-inner-wrapper" style={{ padding: '20px 0' }}>
+                        <h1 className="mb-5 text-2xl font-medium">Faster By Design</h1>
+                        <p className="text-sm">Seamlessly aerodynamic, boldly designed. COBRA AEROJET enables new levels of speed that 
+                            were not believed to be possible until now.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     );
