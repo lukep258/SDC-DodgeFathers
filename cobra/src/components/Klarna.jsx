29dc419023b927c7b3e8537a806d274f04ad784d
WRhee123 <rhee.william@yahoo.com>
2024-01-07 20:27:03 -0500

    added div containing graphs

--- cobra/src/components/Klarna.jsx
+++ cobra/src/components/Klarna.jsx
@@ -24,13 +24,71 @@ return(
         <span className='font-bold text-3xl'>Buy now. Pay with Klarna at</span>
         <span className='font-bold text-3xl mb-2'>your own pace.</span>
         <span className='mb-5'>Get what you love, choose how you pay.</span>
-        <span className='font-semibold relative left-[-200px]'>Pay now</span>
-        <div className='h-16 w-[460px] bg-gray-400 flex flex-row justify-between items-center px-4'>
+        <span className='font-semibold relative left-[-195px]'>Pay now</span>
+        <div className='h-16 w-[460px] bg-gray-400 flex flex-row justify-between items-center px-4 rounded-xl mb-4'>
           <span className='font-semibold text-xl'>$449.00</span>
-          <div className='h-5 w-20 bg-green-400 flex justify-center items-center rounded-xl'>
-            <span className='text-sm'>Pay in full</span>
+          <div className='h-5 w-[70px] bg-green-300 flex justify-center items-center rounded-xl'>
+            <span className='text-[12px] font-medium'>Pay in full</span>
           </div>
         </div>
+        <span className='font-semibold relative left-[-180px]'>Pay over time</span>
+        <div className='h-16 w-[460px] bg-gray-400 flex flex-row justify-between items-center px-4 rounded-tl-xl rounded-tr-xl mb-4'>
+          <div className='flex flex-col w-30 h-12'>
+            <span className='font-semibold text-xl mb-3'>$112.25</span>
+            <span className='text-xs'>Every 14 days</span>
+          </div>
+          <div className='h-5 w-[55px] bg-blue-200 flex justify-center items-center rounded-xl'>
+            <span className='text-[12px] font-medium'>Pay in 4</span>
+          </div>
+        </div>
+        <div className='h-16 w-[460px] bg-gray-400 flex flex-row justify-between items-center px-20 mb-4'>
+          <div className='flex flex-col text-xs items-center'>
+          <svg width="30" height="30" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="15" cy="15" r="15" fill="#E5E5E9"></circle><path d="M15 0C16.9698 2.349e-08 18.9204 0.387987 20.7403 1.14181C22.5601 1.89563 24.2137 3.00052 25.6066 
+      4.3934C26.9995 5.78628 28.1044 7.43987 28.8582 9.25975C29.612 11.0796 30 13.0302 30 15L15 15L15 0Z" fill="#1892F2"></path></svg>
+          <span>112.25</span>
+          <span>Today</span>
+          </div>
+          <div className='flex flex-col text-xs items-center'>
+          <svg width="30" height="30" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="15" cy="15" r="15" fill="#E5E5E9"></circle><path d="M15.5 0C17.4698 2.349e-08 19.4204 0.387987 21.2403 1.14181C23.0601 1.89563 24.7137 3.00052 26.1066 4.3934C27.4995 
+      5.78628 28.6044 7.43987 29.3582 9.25975C30.112 11.0796 30.5 13.0302 30.5 15C30.5 16.9698 30.112 18.9204 29.3582 20.7403C28.6044 
+      22.5601 27.4995 24.2137 26.1066 25.6066C24.7137 26.9995 23.0601 28.1044 21.2402 28.8582C19.4204 29.612 
+      17.4698 30 15.5 30L15.5 15L15.5 0Z" fill="#1892F2"></path></svg>
+          <span>112.25</span>
+          <span>In 2 weeks</span>
+          </div>
+          <div className='flex flex-col text-xs items-center'>
+          <svg width="30" height="30" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="15" cy="15" r="15" fill="#E5E5E9"></circle><path d="M15 0C17.9667 3.53778e-08 20.8668 0.879735 23.3336 2.52796C25.8003 4.17618 27.7229 6.51886 28.8582 9.25975C29.9935 
+      12.0006 30.2906 15.0166 29.7118 17.9264C29.133 20.8361 27.7044 23.5088 25.6066 25.6066C23.5088 27.7044 20.8361 29.133 17.9264 
+      29.7118C15.0166 30.2906 12.0006 29.9935 9.25975 28.8582C6.51886 27.7229 4.17618 25.8003 2.52796 23.3336C0.879734 
+      20.8668 -7.07556e-08 17.9667 0 15L15 15L15 0Z" fill="#1892F2"></path></svg>
+          <span>112.25</span>
+          <span>In 4 weeks</span>
+          </div>
+          <div className='flex flex-col text-xs items-center'>
+          <svg width="31" height="30" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="15.5" cy="15" r="15" fill="#E5E5E9"></circle><circle cx="15.5" cy="15" r="15" fill="#1892F2"></circle></svg>
+          <span>112.25</span>
+          <span>In 6 weeks</span>
+          </div>
+        </div>
+        <div className='h-16 w-[460px] bg-gray-400 flex flex-row justify-between items-center mb-4'>
+          <div className='flex flex-col text-xs items-center'>
+            <span>APR</span>
+            <span className='font-bold'>0%</span>
+          </div>
+          <div className='flex flex-col text-xs items-center'>
+            <span>Interest</span>
+            <span className='font-bold'>Free</span>
+          </div>
+          <div className='flex flex-col text-xs items-center'>
+            <span>Total</span>
+            <span className='font-bold'>$449.00</span>
+          </div>
+        </div>
+        <p>
+          <span> A higher initial payment may be required for some consumers. CA residents: Loans made or arranged pursuant to a California Financing Law license. See payment&nbsp;
+            <a aria-label="terms (Opens in a new window)" class="legal-link underline" href="https://cdn.klarna.com/1.0/shared/content/legal/terms/0/en_us/sliceitinx" target="_blank">terms</a>
+          .</span>
+        </p>
     </div>
 
 
