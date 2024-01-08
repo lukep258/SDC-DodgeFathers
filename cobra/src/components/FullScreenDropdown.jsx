3d44aca26bebaf7059069e324e442183e893ee38
S-Hinkle <shcatchall@protonmail.com>
2024-01-08 15:47:39 -0500

    Adding fonts to NavBar and FullScreenDropdown

--- cobra/src/components/FullScreenDropdown.jsx
+++ cobra/src/components/FullScreenDropdown.jsx
@@ -14,60 +14,60 @@ function FullScreenDropdown({ isVisible, content, onClose }) {
     switch (content) {
         case 'Clubs':
             displayContent = 
-                <div className="flex flex-row justify-center">
+                <div className="flex flex-row justify-center font-titillium font-regular">
                     <ul className="w-[342px] mx-2">
-                        <li><a href='https://www.cobragolf.com/collections/golf-clubs' className="text-slate-600 hover:text-black cursor-pointer">Featured</a></li>
+                        <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs' className="text-slate-600 hover:text-black cursor-pointer font-titillium font-semibold">Featured</a></li>
                         <ul className="border-t-2 border-black">
-                            <li><a href='https://www.cobragolf.com/collections/air-x' className="text-slate-400 hover:text-black cursor-pointer">AIR-X</a></li>
-                            <li><a href='https://www.cobragolf.com/pages/aerojet' className="text-slate-400 hover:text-black cursor-pointer">AEROJET</a></li>
-                            <li><a href='https://www.cobragolf.com/pages/snakebite' className="text-slate-400 hover:text-black cursor-pointer">SKAEBITE Wedges</a></li>
-                            <li><a href='https://www.cobragolf.com/pages/50th-anniversary' className="text-slate-400 hover:text-black cursor-pointer">50th Anniversary</a></li>
-                            <li><a href='https://www.cobragolf.com/pages/king-irons' className="text-slate-400 hover:text-black cursor-pointer">KING Irons</a></li>
-                            <li><a href='https://www.cobragolf.com/pages/king-putters' className="text-slate-400 hover:text-black cursor-pointer">King Putters</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/one-length' className="text-slate-400 hover:text-black cursor-pointer">ONE Length</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/air-x' className="text-slate-400 hover:text-black cursor-pointer">AIR-X</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/pages/aerojet' className="text-slate-400 hover:text-black cursor-pointer">AEROJET</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/pages/snakebite' className="text-slate-400 hover:text-black cursor-pointer">SNAKEBITE Wedges</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/pages/50th-anniversary' className="text-slate-400 hover:text-black cursor-pointer">50th Anniversary</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/pages/king-irons' className="text-slate-400 hover:text-black cursor-pointer">KING Irons</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/pages/king-putters' className="text-slate-400 hover:text-black cursor-pointer">King Putters</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/one-length' className="text-slate-400 hover:text-black cursor-pointer">ONE Length</a></li>
                         </ul>
                     </ul>
                     <ul className="w-[342px] mx-2">
-                        <li><a href='https://www.cobragolf.com/collections/golf-clubs' className="text-slate-600 hover:text-black cursor-pointer">Shop</a></li>
+                        <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs' className="text-slate-600 hover:text-black cursor-pointer font-titillium font-semibold">Shop</a></li>
                         <ul className="border-t-2 border-black">
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-drivers' className="text-slate-400 hover:text-black cursor-pointer">Drivers</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-fairways' className="text-slate-400 hover:text-black cursor-pointer">Fairways</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-hybrids' className="text-slate-400 hover:text-black cursor-pointer">Hybrids</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-sets' className="text-slate-400 hover:text-black cursor-pointer">Complete Sets</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-iron-hybrid-combo-sets' className="text-slate-400 hover:text-black cursor-pointer">Combo Sets</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-irons' className="text-slate-400 hover:text-black cursor-pointer">Irons</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-single-irons' className="text-slate-400 hover:text-black cursor-pointer">Single Irons</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-utility-irons' className="text-slate-400 hover:text-black cursor-pointer">Utility Irons</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-wedges' className="text-slate-400 hover:text-black cursor-pointer">Wedges</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-putters' className="text-slate-400 hover:text-black cursor-pointer">Putters</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs-drivers' className="text-slate-400 hover:text-black cursor-pointer">Drivers</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs-fairways' className="text-slate-400 hover:text-black cursor-pointer">Fairways</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs-hybrids' className="text-slate-400 hover:text-black cursor-pointer">Hybrids</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs-sets' className="text-slate-400 hover:text-black cursor-pointer">Complete Sets</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs-iron-hybrid-combo-sets' className="text-slate-400 hover:text-black cursor-pointer">Combo Sets</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs-irons' className="text-slate-400 hover:text-black cursor-pointer">Irons</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs-single-irons' className="text-slate-400 hover:text-black cursor-pointer">Single Irons</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs-utility-irons' className="text-slate-400 hover:text-black cursor-pointer">Utility Irons</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs-wedges' className="text-slate-400 hover:text-black cursor-pointer">Wedges</a></li>
+                            <li className="pt-2 pb-8"><a href='https://www.cobragolf.com/collections/golf-clubs-putters' className="text-slate-400 hover:text-black cursor-pointer">Putters</a></li>
                         </ul>
                     </ul>
                     <ul className="w-[342px] mx-2">
-                        <li><a href='https://www.cobragolf.com/collections/golf-clubs' className="text-slate-600 hover:text-black cursor-pointer">Clubs For</a></li>
+                        <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs' className="text-slate-600 hover:text-black cursor-pointer font-titillium font-semibold">Clubs For</a></li>
                         <ul className="border-t-2 border-black">
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-womens' className="text-slate-400 hover:text-black cursor-pointer">Womens</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-juniors' className="text-slate-400 hover:text-black cursor-pointer">Juniors</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs-womens' className="text-slate-400 hover:text-black cursor-pointer">Womens</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs-juniors' className="text-slate-400 hover:text-black cursor-pointer">Juniors</a></li>
                         </ul>
                     </ul>
                     <ul className="w-[342px] mx-2">
-                        <li><a href='https://www.cobragolf.com/collections/golf-clubs' className="text-slate-600 hover:text-black cursor-pointer">Collections</a></li>
+                        <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs' className="text-slate-600 hover:text-black cursor-pointer font-titillium font-semibold">Collections</a></li>
                         <ul className="border-t-2 border-black">
-                            <li><a href='https://www.cobragolf.com/collections/aerojet' className="text-slate-400 hover:text-black cursor-pointer">AEROJET</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/king-irons' className="text-slate-400 hover:text-black cursor-pointer">KING Irons</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-wedges' className="text-slate-400 hover:text-black cursor-pointer">SNAKEBITE Wedges</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/ltdx' className="text-slate-400 hover:text-black cursor-pointer">LTDx</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/volition' className="text-slate-400 hover:text-black cursor-pointer">Volition</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/palm-tree-crew' className="text-slate-400 hover:text-black cursor-pointer">Palm Tree Crew</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/t-rail-2023' className="text-slate-400 hover:text-black cursor-pointer">T-RAIL</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/hot-list' className="text-slate-400 hover:text-black cursor-pointer">Hot List</a></li>
-                            <li><a href='https://www.cobragolf.com/collections/golf-clubs-limited-edition' className="text-slate-400 hover:text-black cursor-pointer">Limited Edition</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/aerojet' className="text-slate-400 hover:text-black cursor-pointer">AEROJET</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/king-irons' className="text-slate-400 hover:text-black cursor-pointer">KING Irons</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs-wedges' className="text-slate-400 hover:text-black cursor-pointer">SNAKEBITE Wedges</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/ltdx' className="text-slate-400 hover:text-black cursor-pointer">LTDx</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/volition' className="text-slate-400 hover:text-black cursor-pointer">Volition</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/palm-tree-crew' className="text-slate-400 hover:text-black cursor-pointer">Palm Tree Crew</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/t-rail-2023' className="text-slate-400 hover:text-black cursor-pointer">T-RAIL</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/hot-list' className="text-slate-400 hover:text-black cursor-pointer">Hot List</a></li>
+                            <li className="py-2"><a href='https://www.cobragolf.com/collections/golf-clubs-limited-edition' className="text-slate-400 hover:text-black cursor-pointer">Limited Edition</a></li>
                         </ul>
                     </ul>
                 </div>;
             break;
         case 'Accessories':
             displayContent = 
-                <div  className="flex flex-row justify-center">
+                <div  className="flex flex-row justify-center font-titillium font-regular">
                     <ul className="w-[342px] mx-2">
                         <li><a href='https://www.cobragolf.com/collections/cobra-accessories' className="text-slate-600 hover:text-black cursor-pointer">Featured</a></li>
                         <ul className="border-t-2 border-black">
@@ -112,7 +112,7 @@ function FullScreenDropdown({ isVisible, content, onClose }) {
         // Add cases for other links
         case 'Custom Fitting':
             displayContent = 
-                <div className="flex flex-row items-center h-[70px] mx-60 pb-4">
+                <div className="flex flex-row items-center h-[70px] mx-60 pb-4 font-titillium font-semibold">
                     <a href='https://www.cobragolf.com/collections/golf-clubs-components' className="mr-52 text-slate-600 hover:text-black cursor-pointer">Club Components</a>
                     <a href='https://www.cobragolf.com/pages/find-a-fitter' className="mr-52 text-slate-600 hover:text-black cursor-pointer">Find a Fitter</a>
                     <a href='https://www.cobragolf.com/pages/adjustability-guide' className="mr-52 text-slate-600 hover:text-black cursor-pointer">Adjustability Guide</a>
@@ -121,7 +121,7 @@ function FullScreenDropdown({ isVisible, content, onClose }) {
         case 'On Tour':
             displayContent = 
             <div>
-                <div  className="flex flex-row items-center h-[47px] mx-60">
+                <div  className="flex flex-row items-center h-[47px] mx-60 font-titillium font-semibold">
                     <a href='https://www.cobragolf.com/pages/cobra-tour-staff' className="mr-72 text-slate-600 hover:text-black cursor-pointer">COBRA Tour Staff</a>
                     <a href='https://www.cobragolf.com/pages/tour-tips' className="mr-52 text-slate-600 hover:text-black cursor-pointer">Tour Tips</a>
                 </div>;
@@ -130,7 +130,7 @@ function FullScreenDropdown({ isVisible, content, onClose }) {
     }
 
     return (
-        <div className="fixed inset-x-0 top-0 mt-[81px] border-b-2 bg-white z-50" onClick={handleOutsideClick}>
+        <div className="fixed inset-x-0 top-0 mt-[100px] pt-4 border-b-2 bg-white z-50" onClick={handleOutsideClick}>
             <div>
                 {displayContent}
             </div>
--- cobra/src/components/Navbar.jsx
+++ cobra/src/components/Navbar.jsx
@@ -101,7 +101,7 @@ export default function Navbar() {
                             </a>
                         </div>
                         <div className="navbar-center hidden lg:flex justify-center w-3/5">
-                            <ul className="menu-horizontal custom-navbar">
+                            <ul className="menu-horizontal custom-navbar font-titillium font-semibold">
                             {/* Navbar menu content here */}
                             <li><a onClick={() => handleLinkClick('Clubs')} className="text-base text-black ml-12 cursor-pointer">Clubs</a></li>
                             <li><a onClick={() => handleLinkClick('Accessories')} className="text-base text-black mx-2 cursor-pointer">Accessories</a></li>
@@ -161,27 +161,6 @@ export default function Navbar() {
                     </ul>
                 </div>
             </div>
-
-            <div className="navbar-center custom-navbar font-titillium font-semibold">
-                <a className="text-base text-black mx-2 cursor-pointer">Clubs</a>
-                <a className="text-base text-black mx-2 cursor-pointer">Accessories</a>
-                <a className="text-base text-black mx-2 cursor-pointer">Arccos Caddie</a>
-                <a className="text-base text-black mx-2 cursor-pointer">Custom Fitting</a>
-                <a className="text-base text-black mx-2 cursor-pointer">On Tour</a>
-                <a className="text-base text-black mx-2 cursor-pointer">Sale</a>
-            </div>
-            <div className="navbar-end">
-                <button className="scale-hover mx-2">
-                    <span className="material-symbols-outlined">search</span>
-                </button>
-                <button className="scale-hover mx-2">
-                    <span className="material-symbols-outlined">person</span>
-                </button>
-                <button className="scale-hover mx-2">
-                    <span className="material-symbols-outlined">shopping_bag</span>
-                </button>
-            </div>
-
         </div>
     );
 }
