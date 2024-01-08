0301be160d49cc88584dacd233846a68c8e03b5d
Michael Mote <michael.mote1993@gmail.com>
2024-01-08 11:07:03 -0600

    Remove ReviewHeader component and add newline at end of index.css file

--- cobra/src/App.jsx
+++ cobra/src/App.jsx
@@ -48,7 +48,6 @@ function App() {
                 <GripSpecs />
                 <RelatedProducts/>
                 <RecentlyViewed />
-                <ReviewHeader />
                 <Footer />
             </>
     </>
