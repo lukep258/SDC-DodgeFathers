12dc90c91407a39a6b5b1188172a9924eeddddfd
Patino5 <106916823+Patino5@users.noreply.github.com>
2024-01-08 18:42:41 -0600

    added Fact4 font-family

--- cobra/src/components/Fact4.jsx
+++ cobra/src/components/Fact4.jsx
@@ -4,11 +4,11 @@ const Fact4 = () => {
 
     return (
 
-    <div className="card lg:card-side flex items-center justify-center h- py-24 w-full">
-        <figure className="w-full grow">
+    <div className="card lg:card-side flex items-center font-titillium py-24 w-full">
+        <figure className="grow max-w-3xl mx-auto">
             <img src="http://images.salsify.com/image/upload/s--LE1jbPHA--/ffr9ld5se4hnncjrzrul.png" alt="Unlock Faster Ball Speed"/>
         </figure>
-        <div className="card-body w-full my-auto ">
+        <div className="card-body max-w-xl my-auto font-titillium">
             <h2 className="card-title text-3xl">Unlock Faster Ball Speed</h2>
             <p className="mt-2 leading-7">Forward positioning of the fully suspended PWR-BRIDGE weight allows for maximum energy transfer to unleash faster ball speeds through unrestricted flexibility of the face and sole.</p>
         </div>
