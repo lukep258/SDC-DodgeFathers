07e34736a6a31abe93515b13dc6fc327f4b639e9
Patino5 <106916823+Patino5@users.noreply.github.com>
2024-01-08 19:14:28 -0600

    font family

--- cobra/src/components/ShaftSpecs.jsx
+++ cobra/src/components/ShaftSpecs.jsx
@@ -2,13 +2,13 @@ const ShaftSpecs = () => {
 
 
     return (
-      <div className="mx-12 py-8" >
+      <div className="mx-12 py-8 font-titillium" >
         <div className="border-b mb-7">
-            <a><h1 className="text-3xl cursor-pointer hover:text-blue-700 hover:underline">SHAFT SPECS</h1></a>
+            <a><h1 className="text-3xl cursor-pointer font-semibold hover:text-blue-700 hover:underline">SHAFT SPECS</h1></a>
         </div>
         
   
-        <h3 className="text-lg font-bold mb-2">Project X HZRDUS Gen 4 Black</h3>
+        <h3 className="text-[18px] font-bold mb-2">Project X HZRDUS Gen 4 Black</h3>
   
         <p className="mb-4 text-zinc-600">
           Utilizing a new Dual Torsional Design throughout the shaft, HZRDUS Black Gen 4 creates optimized torques and maximum stability while featuring the classic HZRDUS Black profile of stiff butt, x-stiff midsection, and stiff tip, suited for an aggressive swinger
@@ -19,7 +19,7 @@ const ShaftSpecs = () => {
         </div>
         <div className="table-container overflow-x-auto">
             <table className="table w-full text-nowrap ">
-            <thead className="bg-gray-100 uppercase text-slate-900 text-sm tracking-tight ">
+            <thead className="bg-gray-100 uppercase text-slate-900 text-[16px] tracking-wide ">
                 <tr className="border-t">
                 <th className="font-bold border-b">Model</th>
                 <th className="font-bold border-b">Flex</th>
