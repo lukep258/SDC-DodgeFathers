3252a6be95e4340be2f8685af729986002cc45e4
Patino5 <106916823+Patino5@users.noreply.github.com>
2024-01-08 19:02:49 -0600

    added font family

--- cobra/src/components/TechOverview.jsx
+++ cobra/src/components/TechOverview.jsx
@@ -1,28 +1,28 @@
 const TechOverview = () => {
     return(
         <>
-        <div className='border-b mx-24'>
-            <h1 className='text-3xl hover:text-blue-700 hover:underline'>TECH OVERVIEW</h1>
+        <div className='border-b mx-24 text-gray-600 font-titillium'>
+            <h1 className='text-3xl font-semibold  hover:text-blue-700 hover:underline'>TECH OVERVIEW</h1>
         </div>
-        <div className='flex flex-wrap px-24'>
+        <div className='flex flex-wrap px-24 text-gray-600 font-titillium'>
             <div className='mt-5 mb-9 w-3/6 pr-2'>
-                <h3 className='font-medium'>ADVANCED AERODYNAMIC SHAPING</h3>
+                <h3 className='font-semibold'>ADVANCED AERODYNAMIC SHAPING</h3>
                 <p>A seamless aerodynamic design features a raised skirt, a more symmetrical shape, and streamlined edges to reduce drag for faster clubhead speed.</p>
             </div>
             <div className='mt-5 mb-9 w-3/6'> 
-                <h3 className='font-medium'>PWR-BRIDGE WEIGHTING</h3>
+                <h3 className='font-semibold'>PWR-BRIDGE WEIGHTING</h3>
                 <p>An innovative suspended bridge weight design enables unrestricted flexibility of the face and sole, and positions the CG low and forward to unleash faster ball speed.</p>
             </div>
             <div className='mb-9 w-3/6'>
-                <h3 className='font-medium'>PWRSHELL WITH H.O.T FACE</h3>
+                <h3 className='font-semibold'>PWRSHELL WITH H.O.T FACE</h3>
                 <p>A forged PWRSHELL face insert maximizes flexibility for faster ball speed and higher launch. A H.O.T Face design uses artificial intelligence to create a variable thickness pattern that delivers more efficient speed and spin across the face.</p>
             </div>
             <div className='mb-9 w-3/6'>
-                <h3 className='font-medium'>CARBON CROWN AND SOLE</h3>
+                <h3 className='font-semibold'>CARBON CROWN AND SOLE</h3>
                 <p>A carbon fiber crown and sole create a lighter and stronger chassis and allow more weight to be positioned low in the clubhead for added stability.</p>
             </div>
             <div className='mb-9 w-3/6'>
-                <h3 className='font-medium'>ADJUSTABLE LOFT</h3>
+                <h3 className='font-semibold'>ADJUSTABLE LOFT</h3>
                 <p>An adjustable loft sleeve offers fine tuning of trajectory and spin. Each loft is adjustable up to +/- 1.5 from the printed loft on the head.</p>
             </div>
         </div>
