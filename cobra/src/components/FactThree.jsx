c215424f8cbb0024d3f51bd6d7173d785620c5e4
Patino5 <106916823+Patino5@users.noreply.github.com>
2024-01-08 18:47:19 -0600

    added font family

--- cobra/src/components/FactThree.jsx
+++ cobra/src/components/FactThree.jsx
@@ -2,9 +2,9 @@ const FactThree = () => {
 
 return (
 <>
-<div className="flex flex-wrap justify-center items-center px-10 py-10">
+<div className="flex flex-wrap justify-center items-center px-10 py-10 font-titillium">
     <div className="flex flex-col justify-start w-80 grow mr-20 ml-10">
-        <h1 className ="text-3xl font-medium mb-6">Higher Launch. Optimized Spin.</h1>
+        <h1 className ="text-3xl font-semibold mb-6">Higher Launch. Optimized Spin.</h1>
         <div>
         Proven PWRSHELL L-cup technology maximizes launch and distance, 
         while an A.I. designed H.O.T Face delivers more speed and efficient spin across the entire clubface.
