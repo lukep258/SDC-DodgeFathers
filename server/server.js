ef0a20c4b03233d17b67c1e1cd52019f8a4d3549
Jesse_Carter <jessetcarter@hotmail.com>
2024-01-10 15:22:17 -0700

    GET methods added

--- server/server.js
+++ server/server.js
@@ -18,6 +18,45 @@ app.use(cors());
 
 // METHODS -------
 
+//GET ALL
+app.get(`${URL}`, async (req, res, next) => {
+    try {
+        const result = await pool.query(
+            'SELECT * FROM activities_table'
+        );
+        res.status(200).send(result.rows);
+    }
+    catch (error) {
+        next(error)
+    }
+})
+
+//GET ONE
+app.get(`${URL}/:id`, async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+        console.log(id);
+        if (isNaN(id)) {
+            const error = new Error('Not found');
+            error.status = 404;
+            throw error;
+        }
+        console.log(`${id}`);
+        const result = await pool.query(
+            `SELECT * FROM activities_table WHERE id = ${id}`
+        );
+        if (result.rows.length === 0) {
+            const error = new Error('Not found');
+            error.status = 404;
+            throw error;
+        }
+        res.status(200).send(result.rows);
+    }
+    catch (error) {
+        next(error);
+    }
+})
+
 // LISTENER ------
 
 app.listen(PORT, (req, res) => {
