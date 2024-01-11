6d2d16e382c97794487f26f669f7f2826f87f872
Thiem Huynh <thiem.huynh0@gmail.com>
2024-01-10 19:12:01 -0500

    the seed file had double double "" quotes for json data, replaced with 1 double quote. Now woorks

--- DB/Seed.sql
+++ DB/Seed.sql
@@ -1,5 +1,5 @@
 -- Air X Right
-INSERT INTO club (image_urls, title, price, sale_price, description, hand, loft, custom_options) 
+INSERT INTO club (image_urls, title, price, sale_price, description, left_hand, loft, custom_options) 
 VALUES (
     -- Images
 '[
@@ -23,109 +23,103 @@ VALUES (
  '[9.5, 10.5, 11.5]',
 --  Custon Options
  '{
-  ""ShaftFlex"": [
-    { ""value"": ""Flex-A"", ""disabled"": false },
-    { ""value"": ""Flex-B"", ""disabled"": false },
-    { ""value"": ""Flex-C"", ""disabled"": false },
-    { ""value"": ""Flex-D"", ""disabled"": false }
+  "ShaftFlex": [
+    { "value": "Flex-A", "disabled": false },
+    { "value": "Flex-B", "disabled": false },
+    { "value": "Flex-C", "disabled": false },
+    { "value": "Flex-D", "disabled": false }
   ],
-  ""CustomShaft"": [
-    { ""value"": ""Custom Shaft A"", ""disabled"": false },
-    { ""value"": ""Custom Shaft B"", ""disabled"": false },
-    { ""value"": ""Custom Shaft C"", ""disabled"": false },
-    { ""value"": ""Custom Shaft D"", ""disabled"": false }
+  "CustomShaft": [
+    { "value": "Custom Shaft A", "disabled": false },
+    { "value": "Custom Shaft B", "disabled": false },
+    { "value": "Custom Shaft C", "disabled": false },
+    { "value": "Custom Shaft D", "disabled": false }
   ],
-  ""ShaftLength"": [
-    { ""value"": ""Standard Length"", ""disabled"": false },
-    { ""value"": ""1/4\"" Long"", ""disabled"": false },
-    { ""value"": ""1/2\"" Long"", ""disabled"": false },
-    { ""value"": ""3/4\"" Long"", ""disabled"": false },
-    { ""value"": ""1\"" Long"", ""disabled"": false }
+  "ShaftLength": [
+    { "value": "Standard Length", "disabled": false },
+    { "value": "1/4\" Long", "disabled": false },
+    { "value": "1/2\" Long", "disabled": false },
+    { "value": "3/4\" Long", "disabled": false },
+    { "value": "1\" Long", "disabled": false }
   ],
-  ""GripType"": [
-    { ""value"": ""Standard Grips (Club Connect Not Included)"", ""disabled"": false },
-    { ""value"": ""Club Connect Arccos Grips"", ""disabled"": false }
+  "GripType": [
+    { "value": "Standard Grips (Club Connect Not Included)", "disabled": false },
+    { "value": "Club Connect Arccos Grips", "disabled": false }
   ],
-  ""Grip"": [
-    { ""value"": ""Custom Grip A (+$12)"", ""disabled"": false },
-    { ""value"": ""Custom Grip B (+$18)"", ""disabled"": false },
-    { ""value"": ""Custom Grip C (+$18)"", ""disabled"": false },
-    { ""value"": ""Custom Grip D (+$18)"", ""disabled"": false }
+  "Grip": [
+    { "value": "Custom Grip A (+$12)", "disabled": false },
+    { "value": "Custom Grip B (+$18)", "disabled": false },
+    { "value": "Custom Grip C (+$18)", "disabled": false },
+    { "value": "Custom Grip D (+$18)", "disabled": false }
   ],
-  ""GripOptions"": [
-    { ""value"": ""Option-A"", ""disabled"": false },
-    { ""value"": ""Option-B"", ""disabled"": false },
-    { ""value"": ""Option-C"", ""disabled"": false },
-    { ""value"": ""Option-D"", ""disabled"": false }
+  "GripOptions": [
+    { "value": "Option-A", "disabled": false },
+    { "value": "Option-B", "disabled": false },
+    { "value": "Option-C", "disabled": false },
+    { "value": "Option-D", "disabled": false }
   ]
 }'
-)
+);
 
 -- Air X Left
-INSERT INTO club (image_urls, title, price, sale_price, description, hand, loft, custom_options) 
+INSERT INTO club (image_urls, title, price, sale_price, description, left_hand, loft, custom_options) 
 VALUES (
     -- Images
-'[
-  "https://www.cobragolf.com/cdn/shop/files/kyvziyw6vd3iqaesi5nb_1100x.jpg?v=1703080558",
-  "https://www.cobragolf.com/cdn/shop/files/bq3y2zzxpxs3dlfw0yem_1100x.jpg?v=1703080559",
-  "https://www.cobragolf.com/cdn/shop/files/bsm5rzssh20do99jjuke_1100x.jpg?v=1703080559",
-  "https://www.cobragolf.com/cdn/shop/files/i7tmv9xbuypy922agxb5_1100x.jpg?v=1703080559",
-  "https://www.cobragolf.com/cdn/shop/files/i7tmv9xbuypy922agxb5_1100x.jpg?v=1703080559"
-]',
--- Title
- 'Air-X Offset Driver',
---  Price
- 349.00,
---  Sale Price
- 349.00,
---  Description
- 'The AIR-X OS Driver features a lightweight construction and an all-new H.O.T. Face insert. Designed to help golfers with smooth tempos generate increased club speed and distance with slice-correcting offset for a straighter ball flight.',
---  Hand
- 'False',
---  Loft
- '[9.5, 10.5, 11.5]',
---  Custon Options
- '{
-  ""ShaftFlex"": [
-    { ""value"": ""Flex-A"", ""disabled"": false },
-    { ""value"": ""Flex-B"", ""disabled"": false },
-    { ""value"": ""Flex-C"", ""disabled"": false },
-    { ""value"": ""Flex-D"", ""disabled"": false }
-  ],
-  ""CustomShaft"": [
-    { ""value"": ""Custom Shaft A"", ""disabled"": false },
-    { ""value"": ""Custom Shaft B"", ""disabled"": false },
-    { ""value"": ""Custom Shaft C"", ""disabled"": false },
-    { ""value"": ""Custom Shaft D"", ""disabled"": false }
-  ],
-  ""ShaftLength"": [
-    { ""value"": ""Standard Length"", ""disabled"": false },
-    { ""value"": ""1/4\"" Long"", ""disabled"": false },
-    { ""value"": ""1/2\"" Long"", ""disabled"": false },
-    { ""value"": ""3/4\"" Long"", ""disabled"": false },
-    { ""value"": ""1\"" Long"", ""disabled"": false }
-  ],
-  ""GripType"": [
-    { ""value"": ""Standard Grips (Club Connect Not Included)"", ""disabled"": false },
-    { ""value"": ""Club Connect Arccos Grips"", ""disabled"": false }
-  ],
-  ""Grip"": [
-    { ""value"": ""Custom Grip A (+$12)"", ""disabled"": false },
-    { ""value"": ""Custom Grip B (+$18)"", ""disabled"": false },
-    { ""value"": ""Custom Grip C (+$18)"", ""disabled"": false },
-    { ""value"": ""Custom Grip D (+$18)"", ""disabled"": false }
-  ],
-  ""GripOptions"": [
-    { ""value"": ""Option-A"", ""disabled"": false },
-    { ""value"": ""Option-B"", ""disabled"": false },
-    { ""value"": ""Option-C"", ""disabled"": false },
-    { ""value"": ""Option-D"", ""disabled"": false }
-  ]
-}'
-)
+    '[ "https://www.cobragolf.com/cdn/shop/files/kyvziyw6vd3iqaesi5nb_1100x.jpg?v=1703080558", "https://www.cobragolf.com/cdn/shop/files/bq3y2zzxpxs3dlfw0yem_1100x.jpg?v=1703080559", "https://www.cobragolf.com/cdn/shop/files/bsm5rzssh20do99jjuke_1100x.jpg?v=1703080559", "https://www.cobragolf.com/cdn/shop/files/i7tmv9xbuypy922agxb5_1100x.jpg?v=1703080559", "https://www.cobragolf.com/cdn/shop/files/i7tmv9xbuypy922agxb5_1100x.jpg?v=1703080559" ]',
+    -- Title
+    'Air-X Offset Driver',
+    -- Price
+    349.00,
+    -- Sale Price
+    349.00,
+    -- Description
+    'The AIR-X OS Driver features a lightweight construction and an all-new H.O.T. Face insert. Designed to help golfers with smooth tempos generate increased club speed and distance with slice-correcting offset for a straighter ball flight.',
+    -- Left Hand
+    true,  -- Corrected from 'False'
+    -- Loft
+    '[9.5, 10.5, 11.5]',
+    -- Custom Options
+    '{
+      "ShaftFlex": [
+        { "value": "Flex-A", "disabled": false },
+        { "value": "Flex-B", "disabled": false },
+        { "value": "Flex-C", "disabled": false },
+        { "value": "Flex-D", "disabled": false }
+      ],
+      "CustomShaft": [
+        { "value": "Custom Shaft A", "disabled": false },
+        { "value": "Custom Shaft B", "disabled": false },
+        { "value": "Custom Shaft C", "disabled": false },
+        { "value": "Custom Shaft D", "disabled": false }
+      ],
+      "ShaftLength": [
+        { "value": "Standard Length", "disabled": false },
+        { "value": "1/4\" Long", "disabled": false },
+        { "value": "1/2\" Long", "disabled": false },
+        { "value": "3/4\" Long", "disabled": false },
+        { "value": "1\" Long", "disabled": false }
+      ],
+      "GripType": [
+        { "value": "Standard Grips (Club Connect Not Included)", "disabled": false },
+        { "value": "Club Connect Arccos Grips", "disabled": false }
+      ],
+      "Grip": [
+        { "value": "Custom Grip A (+$12)", "disabled": false },
+        { "value": "Custom Grip B (+$18)", "disabled": false },
+        { "value": "Custom Grip C (+$18)", "disabled": false },
+        { "value": "Custom Grip D (+$18)", "disabled": false }
+      ],
+      "GripOptions": [
+        { "value": "Option-A", "disabled": false },
+        { "value": "Option-B", "disabled": false },
+        { "value": "Option-C", "disabled": false },
+        { "value": "Option-D", "disabled": false }
+      ]
+    }'
+);
 
 -- AERO JET MAX DRIVER Right
-INSERT INTO club (image_urls, title, price, sale_price, description, hand, loft, custom_options) 
+INSERT INTO club (image_urls, title, price, sale_price, description, left_hand, loft, custom_options) 
 VALUES (
     -- Images
 '[
@@ -150,46 +144,46 @@ Headcover and wrench included.',
  '[9.5, 10.5, 12.0]',
 --  Custon Options
  '{
-  ""ShaftFlex"": [
-    { ""value"": ""Flex-E"", ""disabled"": false },
-    { ""value"": ""Flex-F"", ""disabled"": false },
-    { ""value"": ""Flex-G"", ""disabled"": false },
-    { ""value"": ""Flex-H"", ""disabled"": false }
+  "ShaftFlex": [
+    { "value": "Flex-E", "disabled": false },
+    { "value": "Flex-F", "disabled": false },
+    { "value": "Flex-G", "disabled": false },
+    { "value": "Flex-H", "disabled": false }
   ],
-  ""CustomShaft"": [
-    { ""value"": ""Custom Shaft E"", ""disabled"": false },
-    { ""value"": ""Custom Shaft F"", ""disabled"": false },
-    { ""value"": ""Custom Shaft G"", ""disabled"": false },
-    { ""value"": ""Custom Shaft H"", ""disabled"": false }
+  "CustomShaft": [
+    { "value": "Custom Shaft E", "disabled": false },
+    { "value": "Custom Shaft F", "disabled": false },
+    { "value": "Custom Shaft G", "disabled": false },
+    { "value": "Custom Shaft H", "disabled": false }
   ],
-  ""ShaftLength"": [
-    { ""value"": ""1 1/4\" Long"", ""disabled"": false },
-    { ""value"": ""1 1/2\" Long"", ""disabled"": false },
-    { ""value"": ""1/4\" Short"", ""disabled"": false },
-    { ""value"": ""1/2\" Short"", ""disabled"": false },
-    { ""value"": ""3/4\" Short"", ""disabled"": false }
+  "ShaftLength": [
+    { "value": "1 1/4\" Long", "disabled": false },
+    { "value": "1 1/2\" Long", "disabled": false },
+    { "value": "1/4\" Short", "disabled": false },
+    { "value": "1/2\" Short", "disabled": false },
+    { "value": "3/4\" Short", "disabled": false }
   ],
-  ""GripType"": [
-    { ""value"": ""Custom Grips (Club Connect Not Included)"", ""disabled"": false },
-    { ""value"": ""Club Connect Arccos Grips"", ""disabled"": false }
+  "GripType": [
+    { "value": "Custom Grips (Club Connect Not Included)", "disabled": false },
+    { "value": "Club Connect Arccos Grips", "disabled": false }
   ],
-  ""Grip"": [
-    { ""value"": ""Custom Grip E (+$12)"", ""disabled"": false },
-    { ""value"": ""Custom Grip F (+$18)"", ""disabled"": false },
-    { ""value"": ""Custom Grip G (+$18)"", ""disabled"": false },
-    { ""value"": ""Custom Grip H (+$18)"", ""disabled"": false }
+  "Grip": [
+    { "value": "Custom Grip E (+$12)", "disabled": false },
+    { "value": "Custom Grip F (+$18)", "disabled": false },
+    { "value": "Custom Grip G (+$18)", "disabled": false },
+    { "value": "Custom Grip H (+$18)", "disabled": false }
   ],
-  ""GripOptions"": [
-    { ""value"": ""Option-E"", ""disabled"": false },
-    { ""value"": ""Option-F"", ""disabled"": false },
-    { ""value"": ""Option-G"", ""disabled"": false },
-    { ""value"": ""Option-H"", ""disabled"": false }
+  "GripOptions": [
+    { "value": "Option-E", "disabled": false },
+    { "value": "Option-F", "disabled": false },
+    { "value": "Option-G", "disabled": false },
+    { "value": "Option-H", "disabled": false }
   ]
 }'
-)
+);
 
 -- AERO JET MAX DRIVER Left
-INSERT INTO club (image_urls, title, price, sale_price, description, hand, loft, custom_options) 
+INSERT INTO club (image_urls, title, price, sale_price, description, left_hand, loft, custom_options) 
 VALUES (
     -- Images
 '[
@@ -214,41 +208,41 @@ Headcover and wrench included.',
  '[9.5, 10.5, 12.0]',
 --  Custon Options
  '{
-  ""ShaftFlex"": [
-    { ""value"": ""Flex-E"", ""disabled"": false },
-    { ""value"": ""Flex-F"", ""disabled"": false },
-    { ""value"": ""Flex-G"", ""disabled"": false },
-    { ""value"": ""Flex-H"", ""disabled"": false }
+  "ShaftFlex": [
+    { "value": "Flex-E", "disabled": false },
+    { "value": "Flex-F", "disabled": false },
+    { "value": "Flex-G", "disabled": false },
+    { "value": "Flex-H", "disabled": false }
   ],
-  ""CustomShaft"": [
-    { ""value"": ""Custom Shaft E"", ""disabled"": false },
-    { ""value"": ""Custom Shaft F"", ""disabled"": false },
-    { ""value"": ""Custom Shaft G"", ""disabled"": false },
-    { ""value"": ""Custom Shaft H"", ""disabled"": false }
+  "CustomShaft": [
+    { "value": "Custom Shaft E", "disabled": false },
+    { "value": "Custom Shaft F", "disabled": false },
+    { "value": "Custom Shaft G", "disabled": false },
+    { "value": "Custom Shaft H", "disabled": false }
   ],
-  ""ShaftLength"": [
-    { ""value"": ""1 1/4\" Long"", ""disabled"": false },
-    { ""value"": ""1 1/2\" Long"", ""disabled"": false },
-    { ""value"": ""1/4\" Short"", ""disabled"": false },
-    { ""value"": ""1/2\" Short"", ""disabled"": false },
-    { ""value"": ""3/4\" Short"", ""disabled"": false }
+  "ShaftLength": [
+    { "value": "1 1/4\" Long", "disabled": false },
+    { "value": "1 1/2\" Long", "disabled": false },
+    { "value": "1/4\" Short", "disabled": false },
+    { "value": "1/2\" Short", "disabled": false },
+    { "value": "3/4\" Short", "disabled": false }
   ],
-  ""GripType"": [
-    { ""value"": ""Custom Grips (Club Connect Not Included)"", ""disabled"": false },
-    { ""value"": ""Club Connect Arccos Grips"", ""disabled"": false }
+  "GripType": [
+    { "value": "Custom Grips (Club Connect Not Included)", "disabled": false },
+    { "value": "Club Connect Arccos Grips", "disabled": false }
   ],
-  ""Grip"": [
-    { ""value"": ""Custom Grip E (+$12)"", ""disabled"": false },
-    { ""value"": ""Custom Grip F (+$18)"", ""disabled"": false },
-    { ""value"": ""Custom Grip G (+$18)"", ""disabled"": false },
-    { ""value"": ""Custom Grip H (+$18)"", ""disabled"": false }
+  "Grip": [
+    { "value": "Custom Grip E (+$12)", "disabled": false },
+    { "value": "Custom Grip F (+$18)", "disabled": false },
+    { "value": "Custom Grip G (+$18)", "disabled": false },
+    { "value": "Custom Grip H (+$18)", "disabled": false }
   ],
-  ""GripOptions"": [
-    { ""value"": ""Option-E"", ""disabled"": false },
-    { ""value"": ""Option-F"", ""disabled"": false },
-    { ""value"": ""Option-G"", ""disabled"": false },
-    { ""value"": ""Option-H"", ""disabled"": false }
+  "GripOptions": [
+    { "value": "Option-E", "disabled": false },
+    { "value": "Option-F", "disabled": false },
+    { "value": "Option-G", "disabled": false },
+    { "value": "Option-H", "disabled": false }
   ]
 }'
-)
+);
 
